11bba0c6078117d61affcc3cc2c57db06abaa21a
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-13 19:42:52 +0600

    Update server setup with CORS and JSON support

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    }),
+);
+
+export default app;
