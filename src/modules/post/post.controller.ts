786832301353f427435c64b4e40346ca0ff160a9
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-14 22:32:34 +0600

    Implement error handling in post controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,13 @@
 import { RequestHandler } from "express";
 
 const createPost: RequestHandler = async (req, res) => {
-    console.log("Controller");
+    try {
+
+    } catch (error) {
+      return res.status(500).json({
+            success: false,
+      })
+    }
 };
 
 export const postController = {
