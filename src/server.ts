f6eed43e0b54ae2f438888a8079fceb7ae65ebd8
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-14 22:16:34 +0600

    Implement database connection and server listening

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+import app from "./app";
+import { prisma } from "./lib/prisma"
+
 const server = async() =>{
-      return 
+      try {
+            await prisma.$connect();
+            console.log("Successfully comnnected to databases");
+            app.listen(5000, ()=>{
+                  
+            })
+      } catch (error) {
+            
+      } 
 }
