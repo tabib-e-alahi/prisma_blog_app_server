a6d90742ccec8fb115aded8a550d3bed9031a57b
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-14 22:35:34 +0600

    Update post controller to log and return post creation result

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -4,8 +4,12 @@ import { postService } from "./post.service";
 const createPost: RequestHandler = async (req, res) => {
     try {
       const result = await postService.createPost(req.body);
-      
-      res.s
+      console.log(result);
+      return res.status(201).json({
+            success: true,
+            message: "Post created successfully.",
+            data
+      })
     } catch (error: any) {
         return res.status(500).json({
             success: false,
