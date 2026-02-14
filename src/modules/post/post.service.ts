67929914920102cf40f2275b4cd79de4f3269fb0
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-14 22:40:34 +0600

    Update post service to use generated Post type

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,11 +1,12 @@
+import { Post } from "../../generated/prism/client";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async({ title, content }: { title: string; content: string }) => {
+const createPost = async (data: Post) => {
     const result = await prisma.post.create({
-      data: {
-            title, content
-      }
-    })
+        data,
+    });
+
+    return result.send()
 };
 
 export const postService = {
