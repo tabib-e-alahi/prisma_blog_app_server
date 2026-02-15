9ec41acc2776416986ad3cd64eb1648ae77f3b7e
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 12:05:24 +0600

    Update auth middleware to use betterAuth API

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,10 @@
 import {NextFunction, Request, Response} from 'express'
-import {auth as betterAuth} from '../lib/auth.ts'
+import {auth as betterAuth} from '../lib/auth'
 
 const auth = (...roles: any) =>{
       return async(req: Request, res: Response, next: NextFunction) =>{
             //* get user session
-            const session = await auth
+            const session = await betterAuth.api.getSession
             
             next();
       }
