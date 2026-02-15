1d3807b663ce2f32e13007faaf7d766f258816d8
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 19:45:25 +0600

    Refactor getAllPosts to accept payload with search parameter

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -15,7 +15,7 @@ const createPost = async (
     return result;
 };
 
-const getAllPosts = async (search?: string | undefined) => {
+const getAllPosts = async (payload: {search: string | undefined}) => {
     
     const result = await prisma.post.findMany();
     return result;
