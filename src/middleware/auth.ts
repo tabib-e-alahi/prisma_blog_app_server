3355f474855a0bccc3d378170e68aaf48a8a6af6
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 12:25:24 +0600

    Update auth middleware to pass request headers to getSession

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,7 +4,13 @@ import {auth as betterAuth} from '../lib/auth'
 const auth = (...roles: any) =>{
       return async(req: Request, res: Response, next: NextFunction) =>{
             //* get user session
-            const session = await betterAuth.api.getSession
+            const session = await betterAuth.api.getSession({
+                  headers: req.headers as any
+            })
+
+            if(!session){
+                  throw new
+            }
             
             next();
       }
