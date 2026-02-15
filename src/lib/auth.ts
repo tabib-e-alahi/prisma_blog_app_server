6f430da897e1b0633fc3fa79a680a2bb7b3fdafa
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 11:10:20 +0600

    Add nodemailer support for email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,17 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "ssmtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    auth: {
+        user: process.env.APP_USER,
+        pass: process.env.APP_PASS,
+    },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -28,5 +39,12 @@ export const auth = betterAuth({
     trustedOrigins: [process.env.FRONEND_URL!],
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true,
+    },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            
+        },
     },
 });
