30996e2221bc7318a77231fb54ae7c2e10359970
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 11:55:25 +0600

    Update Google OAuth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -123,9 +123,11 @@ export const auth = betterAuth({
         },
     },
     socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID as string, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
+        google: {
+            prompt: "select_account consent",
+            accessType: "offline",
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
     },
 });
