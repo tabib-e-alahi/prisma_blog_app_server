0fae52bd2eb12e15958ee916f2fb0d2b6f212aaa
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 12:30:24 +0600

    Update auth middleware to handle unauthorized and unverified users

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,19 @@
 import {NextFunction, Request, Response} from 'express'
 import {auth as betterAuth} from '../lib/auth'
 
+declare global{
+      namespace Express{
+            interface Request{
+                  user?:{
+                        id: String
+                        email: String
+                        name: Storage
+                        role: 
+                  }
+            }
+      }
+}
+
 const auth = (...roles: any) =>{
       return async(req: Request, res: Response, next: NextFunction) =>{
             //* get user session
@@ -9,7 +22,17 @@ const auth = (...roles: any) =>{
             })
 
             if(!session){
-                  throw new
+                  return res.status(400).json({
+                        success: false,
+                        message: "You are not authorize."
+                  })
+            }
+
+            if(!session.user.emailVerified){
+                 return res.status(403).json({
+                        success: false,
+                        message: "Email verification required. Please verify your email."
+                  }) 
             }
             
             next();
