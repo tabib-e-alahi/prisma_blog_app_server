20d637ff8d81016bc64c8b2d141a1626e50e61b7
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-15 09:28:36 +0600

    Update auth database provider to PostgreSQL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/generated/prisma/client";
+import { prisma } from "./prisma";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
 });
