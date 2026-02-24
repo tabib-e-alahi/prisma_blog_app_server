ec20fa6bc6a5e6e1de86e2084b1b7b53c5b700c4
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 19:26:52 +0600

    Update seedAdmin script with improved logging and formatting

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -3,52 +3,54 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
     try {
-        console.log("***** Admin Seeding Started....")
+        console.log("***** Admin Seeding Started....");
         const adminData = {
             name: "Admin2 Saheb",
             email: "admin2@admin.com",
             role: UserRole.ADMIN,
-            password: "admin1234"
-        }
-        console.log("***** Checking Admin Exist or not")
+            password: "admin1234",
+        };
+        console.log("***** Checking Admin Exist or not");
         // check user exist on db or not
         const existingUser = await prisma.user.findUnique({
             where: {
-                email: adminData.email
-            }
+                email: adminData.email,
+            },
         });
 
         if (existingUser) {
             throw new Error("User already exists!!");
         }
 
-        const signUpAdmin = await fetch("http://localhost:8000/api/auth/sign-up/email", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
+        const signUpAdmin = await fetch(
+            "http://localhost:8000/api/auth/sign-up/email",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(adminData),
             },
-            body: JSON.stringify(adminData)
-        })
-console.log();
-
+        );
+        console.log(signUpAdmin);
 
         if (signUpAdmin.ok) {
-            console.log("**** Admin created")
+            console.log("**** Admin created");
             await prisma.user.update({
                 where: {
-                    email: adminData.email
+                    email: adminData.email,
                 },
                 data: {
-                    emailVerified: true
-                }
-            })
+                    emailVerified: true,
+                },
+            });
 
-            console.log("**** Email verification status updated!")
-            console.log("******* SUCCESS ******")
+            console.log("**** Email verification status updated!");
+            console.log("******* SUCCESS ******");
         }
     } catch (error) {
         console.error(error);
     }
 }
 
-seedAdmin()
+seedAdmin();
