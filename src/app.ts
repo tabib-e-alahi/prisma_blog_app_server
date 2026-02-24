d30ac95f9b0cfebb7547c22175528480f0d5e177
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 14:14:43 +0600

    Remove unused express.json() middleware

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,10 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(express.json());
+
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
-app.use(express.json());
 
 app.use("/posts", postRouter);
 
